980e09140e3d207904a276f104f4feed6a32565e
hsidd1 <hamzasiddiqui247@gmail.com>
2023-04-04 06:52:54 -0400

    reconfiguring as separate proj for each source

---
+++ L4-Duty-Cycle-and-Pulse-Timing/Stepper Motor/stud4b.c
@@ -0,0 +1,57 @@
+// 2DX4_Knowledge_Thread_3_Session_1
+// This program illustrates the use of SysTick in the C language.
+// Note the library headers asscoaited are PLL.h and SysTick.h,
+// which define functions and variables used in PLL.c and SysTick.c.
+// This program uses code directly from your course textbook.
+
+//  Written by Ama Simons
+//  January 18, 2020
+//  Last Update: January 18, 2020
+
+
+#include <stdint.h>
+#include "tm4c1294ncpdt.h"
+#include "PLL.h"
+#include "SysTick.h"
+
+
+
+void PortM_Init(void){
+	//Use PortM pins for output
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R11;				// activate clock for Port N
+	while((SYSCTL_PRGPIO_R&SYSCTL_PRGPIO_R11) == 0){};	// allow time for clock to stabilize
+	GPIO_PORTM_DIR_R |= 0xFF;        								// make PN0 out (PN0 built-in LED1)
+  GPIO_PORTM_AFSEL_R &= ~0xFF;     								// disable alt funct on PN0
+  GPIO_PORTM_DEN_R |= 0xFF;        								// enable digital I/O on PN0
+																									// configure PN1 as GPIO
+  //GPIO_PORTM_PCTL_R = (GPIO_PORTM_PCTL_R&0xFFFFFF0F)+0x00000000;
+  GPIO_PORTM_AMSEL_R &= ~0xFF;     								// disable analog functionality on PN0		
+	return;
+}
+
+void spin(){
+	for(int i=0; i<512; i++){
+		GPIO_PORTM_DATA_R = 0b00001100;
+		SysTick_Wait10ms(1);
+		GPIO_PORTM_DATA_R = 0b00000110;
+		SysTick_Wait10ms(1);
+		GPIO_PORTM_DATA_R = 0b00000011;
+		SysTick_Wait10ms(1);
+		GPIO_PORTM_DATA_R = 0b00001001;
+		SysTick_Wait10ms(1);
+	}
+}
+
+int main(void){
+	PLL_Init();																			// Default Set System Clock to 120MHz
+	SysTick_Init();																	// Initialize SysTick configuration
+	PortM_Init();	
+	spin();
+	return 0;
+}
+
+
+
+
+
+
---
+++ L4-Duty-Cycle-and-Pulse-Timing/Variable Duty Cycle for PWM of an LED/Objects/studio4b.d
@@ -0,0 +1,3 @@
+./objects/studio4b.o: Studio4B.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h tm4c1294ncpdt.h PLL.h \
+  SysTick.h
